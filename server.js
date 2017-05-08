280d62b7e3805e2a6fa57515b7aa3b27751b0a33
tommasoR <tommaso.ruggiero@gmail.com>
2017-05-08 15:30:18 +0200

    versione di produzione

---
+++ server.js
@@ -0,0 +1,6 @@
+/**
+ * Created by 910 on 05/05/2017.
+ * https://medium.com/@ryanchenkie_40935/angular-cli-deployment-host-your-angular-2-app-on-heroku-3f266f13f352
+ * https://github.com/angular/angular-cli
+ *
+ */
